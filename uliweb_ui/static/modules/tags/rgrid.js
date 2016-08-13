d89bb9d2d7bca0cbabe007fef76ebf47ea3c99da
limodou <limodou@gmail.com>
2016-08-13 13:31:41 +0800

    add select2 value_from option

--- uliweb_ui/static/modules/tags/rgrid.js
+++ uliweb_ui/static/modules/tags/rgrid.js
@@ -1,4 +1,4 @@
-riot.tag2('rgrid', '<query-condition if="{has_query}" rules="{query_ules}" fields="{query_fields}" layout="{query_layout}"></query-condition> <rtable cols="{cols}" options="{rtable_options}" data="{data}" start="{start}"></rtable> <div class="clearfix tools"> <pagination if="{pagination}" data="{data}" url="{url}" page="{page}" total="{total}" limit="{limit}" onpagechanged="{onpagechanged}"></pagination> </div>', '', '', function(opts) {
+riot.tag2('rgrid', '<query-condition if="{has_query}" rules="{query_ules}" fields="{query_fields}" layout="{query_layout}"></query-condition> <div if="{tools}" class="rgrid-tools pull-left"> <button each="{btn in tools}" class="{btn.class}" onclick="{btn.onclick}">{btn.label}</button> </div> <rtable cols="{cols}" options="{rtable_options}" data="{data}" start="{start}"></rtable> <div class="clearfix tools"> <pagination if="{pagination}" data="{data}" url="{url}" page="{page}" total="{total}" limit="{limit}" onpagechanged="{onpagechanged}"></pagination> <div if="{footer_tools}" class="pull-right"> <button each="{btn in footer_tools}" class="btn btn-flat btn-sm btn-default" onclick="{btn.onClick}">{btn.label}</button> </div> </div>', 'rgrid .rgrid-tools,[riot-tag="rgrid"] .rgrid-tools,[data-is="rgrid"] .rgrid-tools{margin-bottom:5px;} rgrid .rgrid-tools button,[riot-tag="rgrid"] .rgrid-tools button,[data-is="rgrid"] .rgrid-tools button{margin-right:4px;}', '', function(opts) {
 
 
   var self = this
@@ -16,6 +16,8 @@ riot.tag2('rgrid', '<query-condition if="{has_query}" rules="{query_ules}" field
   this.query_fields = this.query.fields || []
   this.query_layout = this.query.layout || []
   this.start = (this.page - 1) * this.limit
+  this.footer_tools = opts.footer_tools || []
+  this.tools = opts.tools || []
 
   this.rtable_options = {
     theme : opts.theme,
@@ -23,6 +25,7 @@ riot.tag2('rgrid', '<query-condition if="{has_query}" rules="{query_ules}" field
     labelField : opts.labelField || 'title',
     indexCol: opts.indexCol,
     checkCol: opts.checkCol,
+    multiSelect: opts.multiSelect,
     maxHeight: opts.maxHeight,
     minHeight: opts.minHeight,
     height: opts.height,
@@ -31,10 +34,19 @@ riot.tag2('rgrid', '<query-condition if="{has_query}" rules="{query_ules}" field
     container: $(this.root).parent(),
     noData: opts.noData,
     tree: opts.tree,
-    expanded: opts.expanded,
+    expanded: opts.expanded === undefined ? true : opts.expanded,
+    useFontAwesome: opts.useFontAwesome === undefined ? true : opts.useFontAwesome,
     parentField: opts.parentField,
     orderField: opts.orderField,
-    levelField: opts.levelField
+    levelField: opts.levelField,
+    treeField: opts.treeField,
+    onDblclick: opts.onDblclick,
+    onClick: opts.onClick,
+    onMove: opts.onMove,
+    onEdit: opts.onEdit,
+    onEdited: opts.onEdited,
+    draggable: opts.draggable,
+    editable: opts.editable
 
   }
 
@@ -44,6 +56,33 @@ riot.tag2('rgrid', '<query-condition if="{has_query}" rules="{query_ules}" field
   }
 
   this.on('mount', function(){
+    var item
+    for(var i=0, len=this.tools.length; i<len; i++){
+        item = this.tools[i]
+        var action = function(btn) {
+            return function(e) {
+                return btn.onClick.call(self, e)
+            }
+        }
+        item.onclick = action(item)
+        item.class = item.class || 'btn btn-flat btn-sm btn-primary'
+    }
+    this.table = this.root.querySelector('rtable')
+    this.root.add = this.table.add
+    this.root.addFirstChild = this.table.addFirstChild
+    this.root.update = this.table.update
+    this.root.remove = this.table.remove
+    this.root.get = this.table.get
+    this.root.load = this.load
+    this.root.insertBefore = this.table.insertBefore
+    this.root.insertAfter = this.table.insertAfter
+    this.root.get_selected = this.table.get_selected
+    this.root.expand = this.table.expand
+    this.root.collapse = this.table.collapse
+    this.root.is_selected = this.table.is_selected
+    this.root.move = this.table.move
+    this.root.save = this.table.save
+    this.root.diff = this.table.diff
     this.load()
   })
 
@@ -54,6 +93,7 @@ riot.tag2('rgrid', '<query-condition if="{has_query}" rules="{query_ules}" field
       return r.rows
     }).done(function(){
       self.update()
+      self.data.save()
 
     })
   }
