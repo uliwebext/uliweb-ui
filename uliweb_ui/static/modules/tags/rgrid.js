8322364d478032efa2600c1205ea7ab6680f6bd7
limodou <limodou@gmail.com>
2016-11-24 21:20:46 +0800

    Fix

--- uliweb_ui/static/modules/tags/rgrid.js
+++ uliweb_ui/static/modules/tags/rgrid.js
@@ -3,8 +3,6 @@ riot.tag2('rgrid', '<query-condition if="{has_query}" rules="{query_rules}" url=
 
   var self = this
 
-  this.observable = riot.observable()
-
   if (opts.data) {
     if (Array.isArray(opts.data)) {
       this.data = new DataSet(opts.data)
@@ -16,6 +14,7 @@ riot.tag2('rgrid', '<query-condition if="{has_query}" rules="{query_rules}" url=
   this.url = opts.url
 
   var query = new QueryString(this.url)
+  this.observable = opts.observable || riot.observable()
   this.page = opts.page || parseInt(query.get('page')) || 1
   this.limit = opts.limit || 10
   this.total = opts.total || 0
@@ -35,6 +34,7 @@ riot.tag2('rgrid', '<query-condition if="{has_query}" rules="{query_rules}" url=
   this.btn_group_class = opts.btn_group_class || 'btn-group btn-group-sm'
   this.onLoaded = opts.onLoaded
   this.autoLoad = opts.audoLoad || true
+  this.onBeforePage = opts.onBeforePage || function (page) return {}
   this.page_theme = opts.page_theme || 'simple'
 
   this.onsort = function (sorts) {
