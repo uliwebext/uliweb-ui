d4bd350500b2b220f5a26cff7c975f679dbb9fa8
songxianbo.co <songxianbo@ccb.com>
2016-11-24 14:18:38 +0800

    fix bug about jquery.iframe-transport.js convert

--- uliweb_ui/static/modules/jquery.fileupload/jquery.iframe-transport.js
+++ uliweb_ui/static/modules/jquery.fileupload/jquery.iframe-transport.js
@@ -200,14 +200,16 @@
                 if (!!iframe) {
                     var html_str = jQuery(iframe).find('body').html();
                     if (!!html_str) {
-                        var h1_str = html_str.match(/<h1>([\s\S]*)<\/h1>/)[1];
-                        var resp_code = h1_str.split(' ')[0];
-                        var resp_msg = h1_str.replace(resp_code + ' ', '');
-                        throw {'resp_code': resp_code, 'resp_msg': resp_msg};
+                        var match_list = html_str.match(/<h1>([\s\S]*)<\/h1>/);
+                        if ((!!match_list) && (match_list.length > 1)) {
+                            var h1_str = match_list[1];
+                            var resp_code = h1_str.split(' ')[0];
+                            var resp_msg = h1_str.replace(resp_code + ' ', '');
+                            throw {'resp_code': resp_code, 'resp_msg': resp_msg};
+                        }
                     }
-                } else {
-                    return iframe && $.parseJSON($(iframe[0].body).text());
                 }
+                return iframe && $.parseJSON($(iframe[0].body).text());
             },
             'iframe html': function (iframe) {
                 return iframe && $(iframe[0].body).html();
